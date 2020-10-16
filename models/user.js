8d9a7aa40d4586e01bfc63688f503c3ed20d84ed
robergeekhub <robergeekhub@gmail.com>
2020-10-16 20:38:58 +0200

    added the fields that the tables will have in the user.js file in models

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const UserSchema=mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique: true,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    address:{
+        type:Number,
+        required:false
+    },
+    token:{
+        type:String,
+        required:true
+    }
+})
+
+const UserModel = mongoose.model('user',UserSchema);
+
+module.exports = UserModel;
